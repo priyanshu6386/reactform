a65c14bd6bdb929f86bda6ad8dac69a6eeaace1d
priyanshu6386 <priyanshuv624@gmail.com>
2022-11-12 15:51:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ const App = () => {
       id: 1,
       name: "username",
       type: "text",
-      placeholder: "Username",
+      placeholder: "First Name",
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Username",
+      label: "First Name",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true,
     },
